b88a7fd9ec5f8ade50d654457e984cad94f3bf93
semyonDyachenko <semyondyachenko@gmail.com>
2019-12-08 19:22:09 +0300

    Drawable abstract class

---
+++ deps.py
@@ -0,0 +1,5 @@
+ADDITIONAL_LIBRARARIES = {
+    'SFML': ['SFML 2.5.1'],
+    'OpenGL': ['Default Opengl'],
+    'ImGui': ['ImGui SFML']
+}
--- download_script.py
+++
@@ -1,8 +0,0 @@
-import urllib
-
-source = urllib.urlopen("https://github.com/SemyonDyachenko/my_own_game").read()
-
-
-files = {
-
-}
