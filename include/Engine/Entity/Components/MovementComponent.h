a27fe9ec1c48ee277248000ac2bff15f2962581e
семён дьяченко <cemchic05@outlook.com>
2020-02-12 15:48:59 +0300

    MovementComponent

--- include/Engine/Entity/Components/MovementComponent.h
+++ include/Engine/Entity/Components/MovementComponent.h
@@ -2,23 +2,43 @@
 #define ENGINE_MOVEMENT_COMPONENT_H
 
 #include "Component.h"
+#include "../../Math/Vector2.hpp"
+#include "../../renderer/Sprite.h"
 
 namespace Engine
 {
 
+	enum MovementStates
+	{
+		IDLE = 0,
+		MOVING,
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		MOVE_JUMP
+	};
+	
 	class MovementComponent : public Component
 	{
 	public:
-		MovementComponent(float maxVelocity,float acceleration);
+		MovementComponent(Sprite& sprite,float maxVelocity,float acceleration);
 
 		void OnUpdate(float DeltaTime) override;
 
+		const float& GetMaxVelocity() const;
+		const Math::Vec2f& GetVelocity() const;
+
+		
+		const bool GetState(const short unsigned state);
+		
+
 		void Destroy() override;
 
-	public:
-		float m_Velocity;
+	private:
 		float m_Acceleration;
+		float m_Decceleration;
 		float m_MaxVelocity;
+
+		Math::Vec2f m_Velocity;
 	};
 
 }
--- src/Engine/Entity/Components/MovementComponent.cpp
+++ src/Engine/Entity/Components/MovementComponent.cpp
@@ -3,17 +3,37 @@
 namespace Engine
 {
 
-	MovementComponent::MovementComponent(float maxVelocity, float acceleration) : m_MaxVelocity(maxVelocity), m_Acceleration(acceleration)
+
+	MovementComponent::MovementComponent(Sprite& sprite, float maxVelocity, float acceleration) : m_MaxVelocity(maxVelocity), m_Acceleration(acceleration)
 	{
-		m_Velocity = 0.0f;
+		m_Velocity = { 0.0f,0.0f };
 	}
 
 	void MovementComponent::OnUpdate(float DeltaTime)
 	{
+		while(m_Velocity.x <= m_MaxVelocity)
+		{
+			m_Velocity.x += m_Acceleration * DeltaTime;
+			
+		}
+	}
+
+	const float& MovementComponent::GetMaxVelocity() const
+	{
+		return m_MaxVelocity;
+	}
+
+	const Math::Vec2f& MovementComponent::GetVelocity() const
+	{
+		return m_Velocity;
+	}
+
+	const bool MovementComponent::GetState(const unsigned short state)
+	{
 		
 	}
 
-	
+
 	void MovementComponent::Destroy()
 	{
 
